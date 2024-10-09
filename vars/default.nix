05da8ed85e5526c7041fd0a3a1ae399a2be998d3
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2024-10-09 22:12:26 +0000

    alternative way of handling minimal config secrets

--- vars/default.nix
+++ vars/default.nix
@@ -5,33 +5,31 @@
 }: let
   # Default value, can be overridden
   defaultIsMinimal = false;
-in
-  rec {
-    isMinimal = defaultIsMinimal;
 
-    # Conditionally include secrets
-    secrets =
-      if !isMinimal
-      then {
-        inherit
-          (inputs.nix-secrets)
-          userFullName
-          domain
-          email
-          networking
-          ;
-      }
-      else {};
+  # Define secrets function outside the main set
+  getSecrets = isMinimal:
+    if !isMinimal
+    then {
+      inherit
+        (inputs.nix-secrets)
+        userFullName
+        domain
+        email
+        networking
+        ;
+    }
+    else {};
+in rec {
+  isMinimal = defaultIsMinimal;
 
-    username = "dylan";
-    handle = "hierocles";
-    gitHubEmail = "4733259+hierocles@users.noreply.github.com";
+  username = "dylan";
+  handle = "hierocles";
+  gitHubEmail = "4733259+hierocles@users.noreply.github.com";
 
-    # System-specific settings (FIXME: Likely make options)
-    isWork = false; # Used to indicate a host that uses work resources
-    scaling = "1"; # Used to indicate what scaling to use. Floating point number
+  # System-specific settings (FIXME: Likely make options)
+  isWork = false; # Used to indicate a host that uses work resources
+  scaling = "1"; # Used to indicate what scaling to use. Floating point number
 
-    # Merge secrets into the main set
-    # This ensures secrets are included in the output when !isMinimal
-  }
-  // secrets
+  # Include secrets based on isMinimal
+  secrets = getSecrets isMinimal;
+}
