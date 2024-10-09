8e83e2f07156d8177bf5f43707b6964abed00e27
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2024-10-09 20:18:33 +0000

    update git config

--- home/dylan/common/core/git.nix
+++ home/dylan/common/core/git.nix
@@ -8,7 +8,7 @@
 }: let
   handle = configVars.handle;
   publicGitEmail = configVars.gitHubEmail;
-  publicKey = "${config.home.homeDirectory}/.ssh/id_yubikey.pub";
+  publicKey = "${config.home.homeDirectory}/.ssh/dylan.pub";
   username = configVars.username;
 in {
   programs.git = {
@@ -25,9 +25,6 @@ in {
         "ssh://git@github.com" = {
           insteadOf = "https://github.com";
         };
-        "ssh://git@gitlab.com" = {
-          insteadOf = "https://gitlab.com";
-        };
       };
 
       commit.gpgsign = true;
@@ -35,8 +32,6 @@ in {
       user.signing.key = "${publicKey}";
       # Taken from https://github.com/clemak27/homecfg/blob/16b86b04bac539a7c9eaf83e9fef4c813c7dce63/modules/git/ssh_signing.nix#L14
       gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
-
-      save.directory = "${config.home.homeDirectory}/sync/obsidian-vault-01/wiki";
     };
     signing = {
       signByDefault = true;
@@ -51,8 +46,6 @@ in {
   # NOTE: To verify github.com update commit signatures, you need to manually import
   # https://github.com/web-flow.gpg... would be nice to do that here
   home.file.".ssh/allowed_signers".text = ''
-    ${publicGitEmail} ${lib.fileContents (configLib.relativeToRoot "hosts/common/users/${username}/keys/id_maya.pub")}
-    ${publicGitEmail} ${lib.fileContents (configLib.relativeToRoot "hosts/common/users/${username}/keys/id_mara.pub")}
-    ${publicGitEmail} ${lib.fileContents (configLib.relativeToRoot "hosts/common/users/${username}/keys/id_manu.pub")}
+    ${publicGitEmail} ${lib.fileContents (configLib.relativeToRoot "hosts/common/users/${username}/keys/dylan.pub")}
   '';
 }
