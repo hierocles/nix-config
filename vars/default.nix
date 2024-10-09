dd131dde4dfacabaa9c7751df1e322bbb3127cfc
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2024-10-09 22:09:59 +0000

    this should hopefully fix the nixos-installer check errors

--- vars/default.nix
+++ vars/default.nix
@@ -1,18 +1,37 @@
-{inputs, ...}: {
-  inherit
-    (inputs.nix-secrets)
-    userFullName
-    domain
-    email
-    networking
-    ;
+{
+  inputs,
+  lib,
+  ...
+}: let
+  # Default value, can be overridden
+  defaultIsMinimal = false;
+in
+  rec {
+    isMinimal = defaultIsMinimal;
 
-  username = "dylan";
-  handle = "hierocles";
-  gitHubEmail = "4733259+hierocles@users.noreply.github.com";
+    # Conditionally include secrets
+    secrets =
+      if !isMinimal
+      then {
+        inherit
+          (inputs.nix-secrets)
+          userFullName
+          domain
+          email
+          networking
+          ;
+      }
+      else {};
 
-  # System-specific settings (FIXME: Likely make options)
-  isMinimal = false; # Used to indicate nixos-installer build
-  isWork = false; # Used to indicate a host that uses work resources
-  scaling = "1"; # Used to indicate what scaling to use. Floating point number
-}
+    username = "dylan";
+    handle = "hierocles";
+    gitHubEmail = "4733259+hierocles@users.noreply.github.com";
+
+    # System-specific settings (FIXME: Likely make options)
+    isWork = false; # Used to indicate a host that uses work resources
+    scaling = "1"; # Used to indicate what scaling to use. Floating point number
+
+    # Merge secrets into the main set
+    # This ensures secrets are included in the output when !isMinimal
+  }
+  // secrets
