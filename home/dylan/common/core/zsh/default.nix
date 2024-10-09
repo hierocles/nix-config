451bd363411871f82b1e37b0b0aba95a806d389d
Dylan Henrich <4733259+hierocles@users.noreply.github.com>
2024-10-09 20:43:38 +0000

    remove p10k for now

--- home/dylan/common/core/zsh/default.nix
+++ home/dylan/common/core/zsh/default.nix
@@ -16,16 +16,16 @@
     history.share = true;
 
     plugins = [
-      {
-        name = "powerlevel10k-config";
-        src = ./p10k;
-        file = "p10k.zsh";
-      }
-      {
-        name = "zsh-powerlevel10k";
-        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-        file = "powerlevel10k.zsh-theme";
-      }
+      # {
+      #   name = "powerlevel10k-config";
+      #   src = ./p10k;
+      #   file = "p10k.zsh";
+      # }
+      # {
+      #   name = "zsh-powerlevel10k";
+      #   src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+      #   file = "powerlevel10k.zsh-theme";
+      # }
       {
         name = "zsh-term-title";
         src = "${pkgs.zsh-term-title}/share/zsh/zsh-term-title/";
@@ -40,14 +40,14 @@
       }
     ];
 
-    initExtraFirst = ''
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
-    '';
+    # initExtraFirst = ''
+    #   # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+    #   # Initialization code that may require console input (password prompts, [y/n]
+    #   # confirmations, etc.) must go above this block; everything else may go below.
+    #   if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+    #     source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+    #   fi
+    # '';
 
     initExtra = ''
       # autoSuggestions config
