d5d2e0988de2e04e381380f763ad9e9cd21d07ce
hierocles <4733259+hierocles@users.noreply.github.com>
2024-10-11 19:09:05 -0400

    add secrets

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -26,26 +26,23 @@ in {
       sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     };
 
-    # secrets will be output to /run/secrets
-    # e.g. /run/secrets/msmtp-password
-    # secrets required for user creation are handled in respective ./users/<username>.nix files
-    # because they will be output to /run/secrets-for-users and only when the user is assigned to a host.
     secrets = {
-      # For home-manager a separate age key is used to decrypt secrets and must be placed onto the host. This is because
-      # the user doesn't have read permission for the ssh service private key. However, we can bootstrap the age key from
-      # the secrets decrypted by the host key, which allows home-manager secrets to work without manually copying over
-      # the age key.
-      # These age keys are are unique for the user on each host and are generated on their own (i.e. they are not derived
-      # from an ssh key).
       "user_age_keys/${configVars.username}_${config.networking.hostName}" = {
         owner = config.users.users.${configVars.username}.name;
         inherit (config.users.users.${configVars.username}) group;
-        # We need to ensure the entire directory structure is that of the user...
         path = "${homeDirectory}/.config/sops/age/keys.txt";
       };
 
-      # extract username/password to /run/secrets-for-users/ so it can be used to create the user
       "${configVars.username}/password".neededForUsers = true;
+
+      "ssh_keys/dylan" = {};
+      "ssh_keys/id_constellation" = {};
+      "smb-secrets" = {};
+      wireguard = {};
+      transmission = {};
+      "authelia-jwt-secret" = {};
+      "authelia-session-secret" = {};
+      "authelia-storage-encryption-key" = {};
     };
   };
   # The containing folders are created as root and if this is the first ~/.config/ entry,
