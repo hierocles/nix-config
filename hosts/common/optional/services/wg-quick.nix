0938ba76933f9f9d4cab94695fcd33cca3eea310
hierocles <4733259+hierocles@users.noreply.github.com>
2024-10-12 14:56:49 -0400

    update wg config

--- hosts/common/optional/services/wg-quick.nix
+++ hosts/common/optional/services/wg-quick.nix
@@ -3,10 +3,10 @@
     wg0 = {
       configFile = config.sops.secrets.wireguard.path;
       postUp = [
-        "ip route add 10.0.0.0/8 dev wg0" # Route only VPN traffic through WireGuard
+        "ip route add 192.168.15.0/24 dev wg0" # Route only VPN traffic through WireGuard
       ];
       postDown = [
-        "ip route del 10.0.0.0/8 dev wg0"
+        "ip route del 192.168.15.0/24 dev wg0"
       ];
     };
   };
