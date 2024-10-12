c69ce10ff9dd05422b92ab39da88c27b1e40aa65
hierocles <4733259+hierocles@users.noreply.github.com>
2024-10-12 14:10:11 -0400

    rofi gruvbox theme

--- home/dylan/common/optional/desktops/networkmanager.nix
+++ home/dylan/common/optional/desktops/networkmanager.nix
@@ -1,7 +1,7 @@
-{
+{config, ...}: {
   xdg.configFile."networkmanager-dmenu/config.ini".text = ''
     [dmenu]
-    dmenu_command = rofi -dmenu -i -theme nmdm
+    dmenu_command = rofi -dmenu -i -theme ${config.xdg.configHome}/rofi/themes/gruvbox/gruvbox.rasi
     active_chars = ==
     format = {name}  {sec}  {bars}
     list_saved = true
