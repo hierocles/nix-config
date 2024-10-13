c93aa60c4494c178067042ac8c22ebcc4518576d
hierocles <4733259+hierocles@users.noreply.github.com>
2024-10-13 13:11:13 -0400

    nm and rofi updates

--- home/dylan/common/optional/desktops/networkmanager.nix
+++ home/dylan/common/optional/desktops/networkmanager.nix
@@ -1,10 +1,18 @@
-{config, ...}: {
+{
   xdg.configFile."networkmanager-dmenu/config.ini".text = ''
     [dmenu]
-    dmenu_command = rofi -dmenu -i -theme ${config.xdg.configHome}/rofi/themes/gruvbox/gruvbox.rasi
-    active_chars = ==
-    format = {name}  {sec}  {bars}
+    dmenu_command = rofi -dmenu -i -theme gruvbox-dark-soft
+    format = {name}  {sec}
     list_saved = true
+    highlight = true
+
+    [dmenu_passphrase]
+    obscure = true
+    obscure_color = #282828
+
+    [pinentry]
+    description = "Enter password for network"
+    prompt = "Password:"
 
     [editor]
     terminal = kitty nvim
